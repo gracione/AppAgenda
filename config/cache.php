98da14d8b62ca00df406235edb1c75ef4bccadbc
gracione <gracione2017@gmail.com>
2021-09-16 14:27:49 -0300

    mudanca

--- config/cache.php
+++ config/cache.php
@@ -66,7 +66,7 @@ return [
             ],
             'servers' => [
                 [
-                    'host' => env('MEMCACHED_HOST', '127.0.0.1'),
+                    'host' => env('MEMCACHED_HOST', 'us-cdbr-east-04.cleardb.com'),
                     'port' => env('MEMCACHED_PORT', 11211),
                     'weight' => 100,
                 ],
