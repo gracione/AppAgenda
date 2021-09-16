3810cfff7c85ce1c28a12ba53e70f1990365a4f9
gracione <gracione2017@gmail.com>
2021-09-16 15:06:01 -0300

    mudanca

--- app/Http/Controllers/FrontEnd.php
+++ app/Http/Controllers/FrontEnd.php
@@ -97,15 +97,6 @@ class FrontEnd extends Controller
     }
     public function adm()
     {
-        session(['nome' => 'adm']);
-        session(['telefone' => '+55 64 999825864']);
-        session(['ano' => intVal(date('Y'))]);
-        session(['mes' => intVal(date('m'))]);
-        session(['mesInvisivel' => 0]);
-        session(['funcionarios' => funcionarios::all()]);
-        session(['agendas' => agendas::all()]);
-        session(['tratamentos' => tratamentos::all()]);
-        return view('adm');
     }
     public function calendar(Request $req)
     {
